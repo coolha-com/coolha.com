de2d28c37f88c03cf2850f98054c7730026bb80b
Qin Jiangban <tanjiangban@gmail.com>
2025-01-12 20:35:05 +0800

    1/12

--- app/(data)/search/layout.tsx
+++ app/(data)/search/layout.tsx
@@ -20,27 +20,31 @@ export default function layout({ children }) {
         }
     };
     return (
-        <div className="">
+        <div className="w-full">
 
 
-            <div className="fixed z-50 max-w-3xl mx-auto flex justify-center">
-                <div className="navbar w-full py-0 bg-base-100 border-b  ">
-                    <div className=" navbar-start">
-                        <button className="btn btn-ghost btn-square" onClick={() => router.back()}>
-                            <RiArrowLeftLine size={24} />
-                        </button>
-                    </div>
-                    <div className=" navbar-center flex flex-1 w-full ">
-                        <label className="input input-bordered flex flex-1 items-center ">
-                            <input type="text" className="grow" placeholder="搜索" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeyDown} />
+            <div className="fixed z-50 flex  justify-center   ">
+                <div className="">
+                    
+                    <div className="navbar w-full mx-auto max-w-3xl py-0 bg-base-100 border-b">
+                        <div className=" navbar-start">
+                            <button className="btn btn-ghost btn-square" onClick={() => router.back()}>
+                                <RiArrowLeftLine size={24} />
+                            </button>
+                        </div>
+                        <div className=" navbar-center flex flex-1 w-full ">
+                            <label className="input input-bordered flex flex-1 items-center ">
+                                <input type="text" className="grow" placeholder="搜索" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeyDown} />
 
-                        </label>
+                            </label>
+                        </div>
+                        <div className=" navbar-end">
+                            <button className="btn btn-ghost btn-square" onClick={handleButtonClick}>
+                                <RiSearchLine size={24} />
+                            </button>
+                        </div>
                     </div>
-                    <div className=" navbar-end">
-                        <button className="btn btn-ghost btn-square" onClick={handleButtonClick}>
-                            <RiSearchLine size={24} />
-                        </button>
-                    </div>
+
                 </div>
             </div>
 
