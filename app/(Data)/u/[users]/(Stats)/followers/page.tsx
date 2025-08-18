7f7a800de9ecbfbd300e8bb5d04375361d78e932
Qin Jiangban <tanjiangban@gmail.com>
2025-08-19 00:09:20 +0800

    更新 page.tsx

--- app/(Data)/u/[users]/(Stats)/followers/page.tsx
+++ app/(Data)/u/[users]/(Stats)/followers/page.tsx
@@ -1,12 +1,9 @@
-'use client'
-/* import BFollow from '@/components/lnes/DataUsers/hook/BFollow'; */
-
 import Link from 'next/link';
-import { useRouter } from 'next/navigation'
 
 
-export default function followers({ params: { users }, }) {
-  const router = useRouter()
+
+export default function followers() {
+
 
   return (
     <div>
